ed397079bc09caa53a41be9c6a5c861545d3b4dc
Michael Angerman <stormasm@gmail.com>
2014-08-13 18:17:56 -0700

    more work on timesim

--- lib/timesim.rb
+++ lib/timesim.rb
@@ -1,3 +1,6 @@
+require 'active_support/core_ext/date/calculations.rb'
+require 'active_support/core_ext/numeric/time'
+
 require 'yaml'
 
 class TimeSim
@@ -12,7 +15,22 @@ class TimeSim
     puts @config['config']['amqp_server']
   end
 
+  def test
+    time = Time.now
+    puts time
+    puts time.to_i
+    startdate = Date.parse('2012-12-09')
+    puts startdate
+    startime = startdate.to_time
+    puts startime
+    puts startime.to_i
+
+    daysago = 10
+    daysago = daysago.day.ago.to_date
+    puts daysago
+  end
+
 end
 
 mytimesim = TimeSim.new
-mytimesim.gen
+mytimesim.test
