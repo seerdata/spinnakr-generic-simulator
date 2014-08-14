0d049397be32cfa68a450670052b149b5cacfe99
Michael Angerman <stormasm@gmail.com>
2014-08-13 19:03:44 -0700

    more work on timesim

--- lib/timesim.rb
+++ lib/timesim.rb
@@ -9,13 +9,21 @@ class TimeSim
     @config = YAML.load_file("./config.yml") rescue nil || {}
   end
 
-  def gen
+  def init_config
     puts @config
     puts; puts
     puts @config['config']['amqp_server']
   end
 
-  def test
+  def random_int(min, max)
+    rand(max - min) + min
+  end
+
+  def mydays_ago(dayrange)
+    dayrange.day.ago.to_date
+  end
+
+  def testime
     time = Time.now
     puts time
     puts time.to_i
@@ -28,9 +36,28 @@ class TimeSim
     daysago = 10
     daysago = daysago.day.ago.to_date
     puts daysago
+
+    puts mydays_ago(10).to_date
+    puts mydays_ago(-10).to_date
+  end
+
+  def get_random_time_between_two_dates(min_date,max_date)
+    min_date_seconds = min_date.to_time.to_i
+    max_date_seconds = max_date.to_time.to_i
+    random_date_seconds = random_int(min_date_seconds,max_date_seconds)
+    Time.at(random_date_seconds).to_datetime
+  end
+
+  def get_random_time(day_interval)
+    max_date = mydays_ago(-day_interval).to_date
+    min_date = mydays_ago(day_interval).to_date
+    #puts min_date
+    #puts max_date
+    get_random_time_between_two_dates(min_date,max_date)
   end
 
 end
 
-mytimesim = TimeSim.new
-mytimesim.test
+#mytimesim = TimeSim.new
+#mytime = mytimesim.get_random_time(14)
+#puts mytime
