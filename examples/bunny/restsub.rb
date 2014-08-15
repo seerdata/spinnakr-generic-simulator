194e3b5988aff3bf9025f05a139617ef7c3eae92
Michael Angerman <stormasm@gmail.com>
2014-08-14 17:26:17 -0700

    initial commit of restsub

---
+++ examples/bunny/restsub.rb
@@ -0,0 +1,18 @@
+require "bunny"
+require "rest_client"
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+q = channel.queue("generic", :durable => true, :auto_delete => false)
+
+q.subscribe do |delivery_info, properties, payload|
+  puts "[consumer] #{q.name} received a message: #{payload}"
+  response = RestClient.post 'http://localhost:4567/generic/visit/151',
+              :data => payload, :content_type => :json, :accept => :json
+end
+
+sleep 3.5
+puts "Disconnecting..."
+connection.close
