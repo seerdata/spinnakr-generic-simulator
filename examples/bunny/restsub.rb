fbbfb85a77d31cdbbbc8575a44d85cb22ac3fd2a
Michael Angerman <stormasm@gmail.com>
2014-08-17 06:49:13 -0700

    modify api call to new naming convention api/1.0

--- examples/bunny/restsub.rb
+++ examples/bunny/restsub.rb
@@ -9,7 +9,7 @@ q = channel.queue("generic", :durable => true, :auto_delete => false)
 
 q.subscribe do |delivery_info, properties, payload|
   puts "[consumer] #{q.name} received a message: #{payload}"
-  response = RestClient.post 'http://localhost:4567/generic/visit/151',
+  response = RestClient.post 'http://localhost:4567/api/1.0/event/151',
               :data => payload, :content_type => :json, :accept => :json
 end
 
